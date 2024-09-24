ca20513efeb5424daae91e67bdd31ccfe81b94ec
javad <khalilijavad1206@gmail.com>
2024-09-25 00:54:30 +0200

    Update DiffSynth_Studio.py

--- DiffSynth_Studio.py
+++ DiffSynth_Studio.py
@@ -4,7 +4,7 @@ st.set_page_config(layout="wide")
 # Diasble virtual VRAM on windows system
 import torch
 torch.cuda.set_per_process_memory_fraction(0.999, 0)
-0
+
 
 st.markdown("""
 # DiffSynth Studio
